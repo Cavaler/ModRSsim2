cb7ac7b0ea1944ec6be6bf39e23588acc6074170
Pavel Kostromitinov <Pavel.Kostromitinov@wartsila.com>
2023-04-10 12:36:35 +0300

    V8.22.0.1

--- ModRSsim2/About.cpp
+++ ModRSsim2/About.cpp
@@ -206,7 +206,7 @@ CString title;
    m_hyperlink1.SetHoverCursorHandle(AfxGetApp()->LoadCursor(IDC_MYHANDCURSOR));
    //m_hyperlink2.SetHoverCursorHandle(AfxGetApp()->LoadCursor(IDC_MYHANDCURSOR));		// Deleted 2015-07-26 by DL
    // set the URL that we will actually go to here
-   m_hyperlink1.m_link.LoadString(IDS_SFWEBURL);// = "http://www.sourceforge.net/projects/modrssim2/";	// Revised 2015-07-26 by DL
+   m_hyperlink1.m_link.LoadString(IDS_SFWEBURL);
    // set the URL that we will actually go to here
    //m_hyperlink2.m_link = "http://www.modicon.com/";			// Deleted 2015-07-26 by DL
 
